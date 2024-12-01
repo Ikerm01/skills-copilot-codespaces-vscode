1d31574db87668404ace855bed7e90de8553ae0e
Iker Macias Jurado <151910000+Ikerm01@users.noreply.github.com>
2024-12-01 11:14:26 +0000

    test copilot 3

---
+++ comments.js
@@ -0,0 +1,14 @@
+// Create web server
+const express = require('express');
+
+// Create web server using Express
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
